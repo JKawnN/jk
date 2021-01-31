224e4a7be36012ffdf8c829c9d68842aa90d8aa2
JKawnN <auer.sylvain@gmail.com>
2021-01-31 16:25:12 +0100

    base

---
+++ inc/header.tpl.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $titrePage  ?></title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="reset.css">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php
+<?php 
+
+$titrePage = (empty($titrePage)) ? 'JKawn' : $titrePage;
+
+require __DIR__ . '/inc/header.tpl.php';
+require __DIR__ . '/inc/home.tpl.php';
+require __DIR__ . '/inc/footer.tpl.php';

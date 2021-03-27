2bffdb018401b16762accfafc209d62245beea0b
JKawnN <auer.sylvain@gmail.com>
2021-03-27 16:14:06 +0100

    pierre

---
+++ Pierre.php
@@ -0,0 +1,5 @@
+<body style="background-color:black;">
+    <div style="width:100%; height:100%; display: flex; justify-content: center; align-items: center;">
+        <h1 style="color:red; font-size: 7em; text-shadow: 4px 4px 2px rgba(255, 0, 255, 1);">Pierre Yves gros beau gosse va</h1>
+    </div>
+</body>
